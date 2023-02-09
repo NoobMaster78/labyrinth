3218685c6535a70e0f22e342ec8ebf6fbf558261
tim <novoselov_t@list.ru>
2023-02-09 20:26:33 +0700

    Добавьте файлы проекта.

---
+++ labyrinth/MapSite.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace labyrinth
+{
+    public abstract class MapSite
+    {
+        public abstract void Enter();
+    }
+}
---
+++ labyrinth/Wall.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace labyrinth
+{
+    class Wall : MapSite
+    {
+        public Wall()
+        {
+        }
+        public override void Enter()
+        {
+            Console.WriteLine("Wall");
+        }
+    }
+}
